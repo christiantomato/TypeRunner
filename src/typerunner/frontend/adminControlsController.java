92ef285ab51aae5c389226b7e125a1abbe56880a
Sahej Sethi <ssethi55@uwo.ca>
2026-03-24 18:10:17 -0400

    created shell for admin controls

---
+++ src/typerunner/frontend/adminControlsController.java
@@ -0,0 +1,17 @@
+package typerunner.frontend;
+
+import javafx.event.ActionEvent;
+
+public class adminControlsController {
+    public void accountManage (ActionEvent e){
+        System.out.println("Account Management");
+    }
+
+    public void createAccount (ActionEvent e){
+        System.out.println("Create Account");
+    }
+
+    public void adminBack (ActionEvent e){
+        System.out.println("Back Button");
+    }
+}
