f5611f89d6f71329a76ecba217750d453aaa00a5
Tanya Sahota <tsahot@uwo.ca>
2026-03-25 18:08:33 -0400

    Updated audioManager

--- src/typerunner/backend/AudioManager.java
+++ src/typerunner/backend/AudioManager.java
@@ -1,5 +1,28 @@
 package typerunner.backend;
 
 public class AudioManager {
+
+    private double volume;
+    private String currentTrack;
+    private boolean isPlaying;
+
+    public void playMusic() {
+        isPlaying = true;
+    }
+    public void pauseMusic() {
+        isPlaying = false;
+    }
+    public void stopMusic() {
+        isPlaying = false;
+        currentTrack = null;
+
+    }
+    public void setVolume(double volume) {
+        this.volume = volume;
+    }
+    public boolean isPlaying() {
+        return isPlaying;
+    }
     
 }
+
