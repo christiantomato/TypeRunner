3fa3d95fd36702d7db7a33887f9fd80fa9722a20
Christian Tamayo <ctamayo@uwo.ca>
2026-03-25 18:30:57 -0400

    added logic to change displayed text for dropdown

--- src/typerunner/frontend/controllers/AccountCreationController.java
+++ src/typerunner/frontend/controllers/AccountCreationController.java
@@ -2,9 +2,21 @@ package typerunner.frontend.controllers;
 
 import java.io.IOException;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 import typerunner.frontend.ScreenNavigator;
 
 public class AccountCreationController {
+
+    //the account type dropdown
+    @FXML
+    private MenuButton accountType;
+
+    public void selectAccountType(ActionEvent e) throws IOException {
+        MenuItem clicked = (MenuItem) e.getSource();
+        accountType.setText(clicked.getText());
+    }
 
     /**
      * Account Creation
