92dc518b2e8b04a8726ba7785916211ce333c721
Christian Tamayo <ctamayo@uwo.ca>
2026-03-25 14:45:18 -0400

    cleaned up after adding sceneNavigator

--- src/typerunner/frontend/loginController.java
+++ src/typerunner/frontend/loginController.java
@@ -2,24 +2,11 @@ package typerunner.frontend;
 
 import java.io.IOException;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 
 public class loginController {
     
     public void mainMenu(ActionEvent e) throws IOException {
         System.out.println("going back to menu page");
-        switchScene(e, "menu.fxml");
-    }
-
-    //helper method to swtich scenes
-    private void switchScene(ActionEvent e, String fxmlFile) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource(fxmlFile));
-        Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
-        Scene scene = stage.getScene();
-        scene.setRoot(root);
+        sceneNavigator.switchScene(e, "menu.fxml");
     }
 }
