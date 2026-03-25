a3b60986f029cef7b330d55d2fc94e193e191de7
Christian Tamayo <ctamayo@uwo.ca>
2026-03-25 14:45:07 -0400

    back button logic

--- src/typerunner/frontend/adminPasskeyController.java
+++ src/typerunner/frontend/adminPasskeyController.java
@@ -1,5 +1,13 @@
 package typerunner.frontend;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+
 public class adminPasskeyController {
     
+    public void mainMenu(ActionEvent e) throws IOException {
+        sceneNavigator.switchScene(e, "menu.fxml");
+    }
+
 }
