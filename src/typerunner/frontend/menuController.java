ed2b1d27530facd6805b13df4de3439a4a57883b
Christian Tamayo <ctamayo@uwo.ca>
2026-03-25 14:45:30 -0400

    cleaned up after adding sceneNavigator

--- src/typerunner/frontend/menuController.java
+++ src/typerunner/frontend/menuController.java
@@ -2,17 +2,14 @@ package typerunner.frontend;
 
 import java.io.IOException;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class menuController {
 
     public void loginPage(ActionEvent e) throws IOException {
         System.out.println("going to login page");
-        switchScene(e, "login.fxml");
+        sceneNavigator.switchScene(e, "login.fxml");
     }
 
      public void viewLeaderboard(ActionEvent e) {
@@ -25,7 +22,7 @@ public class menuController {
     
     public void adminControls(ActionEvent e) throws IOException{
         System.out.println("admin");
-        switchScene(e, "adminPasskey.fxml");
+        sceneNavigator.switchScene(e, "adminPasskey.fxml");
     }
     
     public void exit(ActionEvent e) throws IOException {
@@ -34,13 +31,5 @@ public class menuController {
         Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
         //close it
         stage.close();
-    }
-
-    //helper method to swtich scenes
-    private void switchScene(ActionEvent e, String fxmlFile) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource(fxmlFile));
-        Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
-        Scene scene = stage.getScene();
-        scene.setRoot(root);
     }
 }
