459f6d09a6c48864745a0ec271597ab5890f74ab
Christian Tamayo <ctamayo@uwo.ca>
2026-03-25 20:54:46 -0400

    code to modify creation message

--- src/typerunner/frontend/controllers/AccountCreationController.java
+++ src/typerunner/frontend/controllers/AccountCreationController.java
@@ -5,16 +5,20 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
+import javafx.scene.text.Text;
 import typerunner.frontend.ScreenNavigator;
 
 public class AccountCreationController {
 
-    //the account type dropdown
-    @FXML
-    private MenuButton accountType;
+    @FXML private MenuButton accountType; //changes value for dropdown
+    @FXML private Text creationMessage; //feedback for user on creation status
 
     public void selectAccountType(ActionEvent e) throws IOException {
+        System.out.println("selecting account type");
+
+        //get which menu item was clicked
         MenuItem clicked = (MenuItem) e.getSource();
+        //set the corresponding text
         accountType.setText(clicked.getText());
     }
 
@@ -26,13 +30,17 @@ public class AccountCreationController {
      */
 
     public void createAccount(ActionEvent e) throws IOException {
+        System.out.println("creating account");
+
         /*
         get the strings from the textfields
         pass it into the corresponding function
         show confirmation based on return value (true or false)
         */
 
-        System.out.println("creating account");
+        //set the message based on whether successful or not!
+        creationMessage.setText("Successfully Created");
+
     }
     
     public void returnToAdminControls(ActionEvent e) throws IOException {
