9b05030aa64885968f898919f7d5b58ce8a12f3e
Christian Tamayo <ctamayo@uwo.ca>
2026-03-25 21:09:14 -0400

    start work on account management

---
+++ src/typerunner/frontend/controllers/AccountManagementController.java
@@ -0,0 +1,13 @@
+package typerunner.frontend.controllers;
+
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import typerunner.frontend.ScreenNavigator;
+
+public class AccountManagementController {
+    
+    public void returnToAdminControls(ActionEvent e) throws IOException {
+        System.out.println("returning to admin controls");
+        ScreenNavigator.switchScene(e, "fxml/admin-controls.fxml");
+    }
+}
