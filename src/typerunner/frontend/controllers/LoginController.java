617730e9e8303f30034f7d95a901a5be13cd5b3a
Christian Tamayo <ctamayo@uwo.ca>
2026-03-27 14:58:27 -0400

    added try catch and nicely polished

--- src/typerunner/frontend/controllers/LoginController.java
+++ src/typerunner/frontend/controllers/LoginController.java
@@ -1,6 +1,5 @@
 package typerunner.frontend.controllers;
 
-import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
@@ -29,47 +28,57 @@ public class LoginController {
      * login method in the backend. Sends user to player screen if successful.
      * 
      * @param e the button event
-     * @throws IOException if there is an input/output problem
     */
 
     @FXML
-    private void login(ActionEvent e) throws IOException {
+    private void login(ActionEvent e) {
+        try {
+            System.out.println("pressed login");
 
-        //get the username and password
-        String username = usernameField.getText();
-        String password = passwordField.getText();
+            //get the username and password
+            String username = usernameField.getText();
+            String password = passwordField.getText();
 
-        //print for debugging purposes
-        System.out.println(username);
-        System.out.println(password);
+            //print for debugging purposes
+            System.out.println(username);
+            System.out.println(password);
 
-        //make sure they are not empty
-        if(username.isBlank() || password.isBlank()) {
-            System.out.println("a field is empty");
-            return;
+            //make sure they are not empty
+            if(username.isBlank() || password.isBlank()) {
+                System.out.println("a field is empty");
+                return;
+            }
+
+            /*
+            boolean successful = backend.login(username, password);
+            if successful, switch scenes, otherwise provide feedback for invalid login.
+            */
+            
+            //ScreenNavigator.switchScene(e, "fxml/player-screen.fxml");
+        } 
+
+        catch (Exception exception) {
+            System.out.println("exception while logging in:\n" + exception);
         }
-
-        /*
-        boolean successful = backend.login(username, password);
-        if successful, switch scenes, otherwise provide feedback for invalid login.
-        */
-        
-        //ScreenNavigator.switchScene(e, "fxml/player-screen.fxml");
     }
     
-
     /**
      * Return to the Main Menu
      * 
      * Takes the user back to the main menu. 
      * 
      * @param e the button event
-     * @throws IOException if there is an input/output problem
      */
 
     @FXML
-    private void returnToMenu(ActionEvent e) throws IOException {
-        System.out.println("going back to menu page");
-        ScreenNavigator.switchScene(e, "fxml/menu.fxml");
+    private void returnToMenu(ActionEvent e) {
+        try {
+            System.out.println("going back to menu page");
+            ScreenNavigator.switchScene(e, "fxml/menu.fxml");
+        } 
+
+        catch (Exception exception) {
+            System.out.println("exception going back to menu:\n" + exception);
+        }
     }
 }
