ffae65bfbb0e1ec9287d019368dc753298205c61
Christian Tamayo <ctamayo@uwo.ca>
2026-03-27 15:02:49 -0400

    try catch and javadocs and nice

--- src/typerunner/frontend/controllers/AdminPasskeyController.java
+++ src/typerunner/frontend/controllers/AdminPasskeyController.java
@@ -1,6 +1,5 @@
 package typerunner.frontend.controllers;
 
-import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
@@ -27,31 +26,37 @@ public class AdminPasskeyController {
      * to the controls page. 
      * 
      * @param e the button event
-     * @throws IOException if there is an input/output problem
      */
 
     @FXML
-    private void validatePasskey(ActionEvent e) throws IOException {
+    private void validatePasskey(ActionEvent e) {
+        try {
+            System.out.println("pressed validate");
 
-        //get the inputted passkey
-        String passkey = passkeyField.getText();
+            //get the inputted passkey
+            String passkey = passkeyField.getText();
 
-        //print for debugging purposes
-        System.out.println(passkey);
+            //print for debugging purposes
+            System.out.println(passkey);
 
-        //make sure its not blank
-        if(passkey.isBlank()) {
-            System.out.println("passkey field is blank"); 
-            return;
+            //make sure its not blank
+            if(passkey.isBlank()) {
+                System.out.println("passkey field is blank"); 
+                return;
+            }
+
+            /*
+            boolean successful = backend.passkey(passkey);
+            if true, navigate to the admin controls...
+            */
+        
+            System.out.println("going to admin controls");
+            ScreenNavigator.switchScene(e, "fxml/admin-controls.fxml");
+        } 
+
+        catch (Exception exception) {
+            System.out.println("exception while validating passkey:\n" + exception);
         }
-
-        /*
-        boolean successful = backend.passkey(passkey);
-        if true, navigate to the admin controls...
-        */
-       
-        System.out.println("going to admin controls");
-        ScreenNavigator.switchScene(e, "fxml/admin-controls.fxml");
     }
     
     /**
@@ -60,12 +65,18 @@ public class AdminPasskeyController {
      * takes the user back to the main menu. 
      * 
      * @param e the button event
-     * @throws IOException if there is an input/output problem
      */
 
     @FXML
-    private void mainMenu(ActionEvent e) throws IOException {
-        ScreenNavigator.switchScene(e, "fxml/menu.fxml");
+    private void returnToMenu(ActionEvent e) {
+        try {
+            System.out.println("going back to menu page");
+            ScreenNavigator.switchScene(e, "fxml/menu.fxml");
+        } 
+
+        catch (Exception exception) {
+            System.out.println("exception going back to menu:\n" + exception);
+        }
     }
 
     /**
