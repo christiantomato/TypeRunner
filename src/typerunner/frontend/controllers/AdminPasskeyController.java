0faa04d18a29fe2c9d60c2bf4f9ca75a029170c2
Christian Tamayo <ctamayo@uwo.ca>
2026-03-27 14:45:09 -0400

    extract the inputted passkey and sketch out logic

--- src/typerunner/frontend/controllers/AdminPasskeyController.java
+++ src/typerunner/frontend/controllers/AdminPasskeyController.java
@@ -2,28 +2,84 @@ package typerunner.frontend.controllers;
 
 import java.io.IOException;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
 import typerunner.frontend.ScreenNavigator;
+
+/**
+ * Controller for the Passkey Page
+ * 
+ * Handles the GUI events on this page. Works with backend to
+ * allow access to the administrator controls screen.
+ * 
+ * @author Christian Tamayo
+ */
 
 public class AdminPasskeyController {
 
-    public void validatePasskey(ActionEvent e) throws IOException {
+    /** the passkey passwordfield*/
+    @FXML private PasswordField passkeyField;
+
+    /**
+     * Passkey Verification
+     * 
+     * Checks the inputted passkey to determine whether to allow access
+     * to the controls page. 
+     * 
+     * @param e the button event
+     * @throws IOException if there is an input/output problem
+     */
+
+    @FXML
+    private void validatePasskey(ActionEvent e) throws IOException {
+
+        //get the inputted passkey
+        String passkey = passkeyField.getText();
+
+        //print for debugging purposes
+        System.out.println(passkey);
+
+        //make sure its not blank
+        if(passkey.isBlank()) {
+            System.out.println("passkey field is blank"); 
+            return;
+        }
 
         /*
-        get the passkey from the textfield
-        pass it into the corresponding method
-        if true, navigate to the admin controls
+        boolean successful = backend.passkey(passkey);
+        if true, navigate to the admin controls...
         */
        
         System.out.println("going to admin controls");
         ScreenNavigator.switchScene(e, "fxml/admin-controls.fxml");
     }
     
-    public void mainMenu(ActionEvent e) throws IOException {
+    /**
+     * Return to the Main Menu
+     * 
+     * takes the user back to the main menu. 
+     * 
+     * @param e the button event
+     * @throws IOException if there is an input/output problem
+     */
+
+    @FXML
+    private void mainMenu(ActionEvent e) throws IOException {
         ScreenNavigator.switchScene(e, "fxml/menu.fxml");
     }
 
-    //TODO: toggle the visibility of the textfield
-    public void togglePasskey(ActionEvent e) {
-        System.out.println("toggles visibility");
+    /**
+     * Toggles Passkey Visibility
+     * 
+     * shows the passkey being inputted using the checkbox. 
+     * 
+     * @TODO: IMPLEMENT IT 
+     * 
+     * @param e
+     */
+
+    @FXML
+    private void togglePasskey(ActionEvent e) {
+        System.out.println("toggling visibility");
     }
 }
