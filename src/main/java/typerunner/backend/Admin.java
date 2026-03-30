f5c948820d9037b8c69316cfac4600bbb3da4837
Christian Tamayo <ctamayo@uwo.ca>
2026-03-30 16:43:37 -0400

    removed unecessary things

--- src/main/java/typerunner/backend/Admin.java
+++ src/main/java/typerunner/backend/Admin.java
@@ -1,22 +1,8 @@
 package typerunner.backend;
 
 public class Admin extends Player {
-
-    private String passKeyHash;
     
-    public Admin(String username, String password, String passKey) {
-        super(username, password, "admin@typerunner.com", true); // Admins use their password as the passKey for simplicity
-        this.passKeyHash = passKey;
+    public Admin(String username, String password) {
+        super(username, password);
     }
-
-    public boolean validatePassKey(String inputKey) {
-        return this.passKeyHash.equals(inputKey);
-    }
-
-    public void resetHighScores(Player player) {
-        player.resetStats();
-    }
-
-
-    
 }
