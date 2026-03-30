26063af8e5fc176b6c13b837b3ff92980503b316
Noh Adhanom Woldetinsae <nwoldeti@uwo.ca>
2026-03-30 18:06:40 -0400

    testing

--- src/main/java/typerunner/backend/AccountManager.java
+++ src/main/java/typerunner/backend/AccountManager.java
@@ -8,40 +8,49 @@ import java.io.Reader;
 import java.io.Writer;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 
 /**
  * Account Manager
- * 
- * This class handles all the logic with reading and writing from the player database.
- * There is only ever one instance of this class.
- * 
+ *
+ * This class handles all the logic with reading and writing from the player
+ * database. There is only ever one instance of this class.
+ *
  * @author Olorunfemi Martins Kayode
  * @author Christian Tamayo
  */
-
 public class AccountManager {
-    /** the singular instance of this class */
+
+    /**
+     * the singular instance of this class
+     */
     private static AccountManager instance;
-    /** the list of player account */
+    /**
+     * the list of player account
+     */
     private ArrayList<Player> accounts;
-    /** the database file */
+    /**
+     * the database file
+     */
     private File databaseFile;
-    /** not sure what a Gson is */
+    /**
+     * not sure what a Gson is
+     */
     private final Gson gson;
 
     /**
      * Account Manager Constructor
-     * 
-     * The constructor for the account manager. It is private so no other classes can build it, 
-     * as we want only one instance throughout the program. 
-     * 
+     *
+     * The constructor for the account manager. It is private so no other
+     * classes can build it, as we want only one instance throughout the
+     * program.
+     *
      * @param filePath the json file storing account information
      */
-
-    private AccountManager(String filePath) { 
+    private AccountManager(String filePath) {
         this.databaseFile = new File(filePath);
         // Pretty printing makes the JSON file human-readable
         this.gson = new GsonBuilder().setPrettyPrinting().create();
@@ -51,18 +60,17 @@ public class AccountManager {
 
     /**
      * Get Instance
-     * 
-     * Returns the instance of the account manager. If it has not been initialized yet, 
-     * it calls the constructor and builds it. 
-     * Static so we can call it from anywhere to get the account manager instance. 
-     * 
+     *
+     * Returns the instance of the account manager. If it has not been
+     * initialized yet, it calls the constructor and builds it. Static so we can
+     * call it from anywhere to get the account manager instance.
+     *
      * @return the instance of the account manager
      * @see AccountManager
      */
-
     public static AccountManager getInstance() {
         //create an instance if there isn't one
-        if(instance == null) {
+        if (instance == null) {
             instance = new AccountManager("accounts.json");
         }
         return instance;
@@ -70,18 +78,16 @@ public class AccountManager {
 
     /**
      * Set Player Under Admin
-     * 
+     *
      * Adds the player to the list of players for the admin.
      *
      */
-
     public void setPlayerUnderAdmin(String adminUsername, String playerUsername) {
         Admin admin = (Admin) findPlayer(adminUsername);
         admin.addPlayer(playerUsername);
     }
 
     // --- UML PUBLIC METHODS ---
-
     public boolean resetStats(Player player) {
         if (player != null) {
             player.resetStats();
@@ -102,26 +108,24 @@ public class AccountManager {
 
     /**
      * Create Account Method
-     * 
+     *
      * Creates either a player or admin account.
-     * 
+     *
      * @param username the username
      * @param password the password
      * @param isAdmin admin account if true, player if false
      * @return true if account creation was successful
      */
-
     public boolean createAccount(String username, String password, boolean isAdmin) {
         //make sure a player with that username does not already exist
-        if(findPlayer(username) == null) {
+        if (findPlayer(username) == null) {
             //create admin account
-            if(isAdmin) {
+            if (isAdmin) {
                 Admin newAdmin = new Admin(username, password);
                 accounts.add(newAdmin);
                 saveAccounts();
                 return true;
-            }
-            //create a regular player account
+            } //create a regular player account
             else {
                 Player newPlayer = new Player(username, password);
                 accounts.add(newPlayer);
@@ -132,8 +136,10 @@ public class AccountManager {
         return false;
     }
 
-    public Player findPlayer(String username) { 
-        if (username == null) return null;
+    public Player findPlayer(String username) {
+        if (username == null) {
+            return null;
+        }
         for (Player p : accounts) {
             // Logic for entering it irrespective of case
             if (p.getUsername().equalsIgnoreCase(username)) {
@@ -143,11 +149,14 @@ public class AccountManager {
         return null;
     }
 
-    public void loadAccounts() { 
-        if (!databaseFile.exists()) return;
+    public void loadAccounts() {
+        if (!databaseFile.exists()) {
+            return;
+        }
 
         try (Reader reader = new FileReader(databaseFile)) {
-            Type listType = new TypeToken<ArrayList<Player>>(){}.getType();
+            Type listType = new TypeToken<ArrayList<Player>>() {
+            }.getType();
             ArrayList<Player> loadedData = gson.fromJson(reader, listType);
             if (loadedData != null) {
                 this.accounts = loadedData;
@@ -159,29 +168,28 @@ public class AccountManager {
 
     /**
      * Save Accounts to Database
-     * 
+     *
      * Writes the data from the accounts list into pretty string in the json.
-     * 
+     *
      */
-
-    public void saveAccounts() { 
+    public void saveAccounts() {
         try {
             Writer writer = new FileWriter(databaseFile);
             gson.toJson(accounts, writer);
-        } 
-
-        catch(IOException e) {
+        } catch (IOException e) {
             System.err.println("Error saving JSON: " + e);
         }
     }
 
-    public void findPlayerByAdmin(Admin admin){ // return a list of players that are managed by the admin
+    public void findPlayerByAdmin(Admin admin) { // return a list of players that are managed by the admin
 
-        
         // go over the list of players
         //check who their administrator is
         //if it is the admin we are looking for, add them to the list of players
-
-
     }
+
+    public ArrayList<Player> getAccounts() {
+        return accounts;
+    }
+
 }
---
+++ src/main/java/typerunner/backend/Main.java
@@ -0,0 +1,31 @@
+package typerunner.backend;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // Create an instance of AccountManager
+        AccountManager accountManager = AccountManager.getInstance();
+
+        // Create some accounts
+      //  accountManager.createAccount("admin1", "password123", true, "admin1-id");
+        //accountManager.createAccount("player1", "password456", false, "player1-id");
+        //accountManager.createAccount("player2", "password789", , true);
+
+        accountManager.createAccount("admin1", "password123", true);
+        accountManager.createAccount("player1", "password456", false);
+
+        accountManager.getAccounts();
+
+        //accountManager.getAccounts();
+        //accountManager.getAccounts()
+
+
+ 
+
+        // Save accounts to JSON
+        //accountManager.saveAccountsToJson();
+    }
+
+    
+    
+}
