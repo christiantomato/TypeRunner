24c23c4cd614cc13815ddbfbc470da41ed8fbf5b
Christian Tamayo <ctamayo@uwo.ca>
2026-03-30 17:16:08 -0400

    finished a decent admin class

--- src/main/java/typerunner/backend/Admin.java
+++ src/main/java/typerunner/backend/Admin.java
@@ -1,8 +1,35 @@
 package typerunner.backend;
 
+import java.util.ArrayList;
+
+/**
+ * Administrator Class
+ * 
+ * Defines the properties of an administrator. They supervise any amount of players,
+ * and can play the game as normal. 
+ * 
+ * @author Christian Tamayo
+ */
+
 public class Admin extends Player {
-    
+    /** a list of all the players they manage */
+    private ArrayList<Player> players; 
+
+    /**
+     * Admin Constructor
+     * 
+     * Passes in the username and password, and initalizes an empty list
+     * where players can be added into upon creation of their accounts. 
+     * 
+     * @param username the username of the admin
+     * @param password the password of the admin
+     */
+
     public Admin(String username, String password) {
+        //pass into super
         super(username, password);
+
+        //initalize an empty list
+        this.players = new ArrayList<>();
     }
 }
