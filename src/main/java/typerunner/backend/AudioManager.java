07c30bcee851c1cad962d0035c3b789a86d0378d
Tanya Sahota <tsahot@uwo.ca>
2026-03-30 17:49:20 -0400

    Updated AudioManager

--- src/main/java/typerunner/backend/AudioManager.java
+++ src/main/java/typerunner/backend/AudioManager.java
@@ -1,28 +1,50 @@
 package typerunner.backend;
+/**
+ * The AudioManager class handles audio playback functionality.
+ * This includes playing, pausing, stopping music, and controlling volume.
+ * 
+ * @author Tanya Sahota
+ * @version 1.0
+ */
 
 public class AudioManager {
-
+/** The current volume level of the audio. */
     private double volume;
+/** The name of the current track being played. */
     private String currentTrack;
+/** Whether an audio is currently playing or not. */
     private boolean isPlaying;
-
+/** Starts to play music and sets isPlaying state to true. */
     public void playMusic() {
         isPlaying = true;
     }
+/** Pauses the music and sets isPlaying state to false. */
     public void pauseMusic() {
         isPlaying = false;
     }
+/** Stops the music and sets isPlaying state to false and currentTrack to null. */
     public void stopMusic() {
         isPlaying = false;
         currentTrack = null;
 
     }
+/** Sets the volume level.
+ * 
+ * @param volume the new volume level to set.
+ */
     public void setVolume(double volume) {
         this.volume = volume;
     }
+/** Checks if music is currently playing.
+ * 
+ * @return true if music is playing, false otherwise.
+ * @return
+ */
     public boolean isPlaying() {
         return isPlaying;
     }
     
 }
+
+
 
