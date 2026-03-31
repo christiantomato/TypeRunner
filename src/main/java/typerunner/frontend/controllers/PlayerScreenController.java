a2c0779d3877c27f330e00e2da1926864480a3dc
Christian Tamayo <ctamayo@uwo.ca>
2026-03-31 18:01:27 -0400

    login working

--- src/main/java/typerunner/frontend/controllers/PlayerScreenController.java
+++ src/main/java/typerunner/frontend/controllers/PlayerScreenController.java
@@ -2,9 +2,45 @@ package typerunner.frontend.controllers;
 
 import java.io.IOException;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+import typerunner.backend.LoginManager;
 import typerunner.frontend.ScreenNavigator;
 
+/**
+ * Player Screen Controller
+ * 
+ * Handles logic on this page. 
+ * 
+ * @author Sahej Singh
+ * @author Christian Tamayo
+ */
+
 public class PlayerScreenController {
+
+    /** the text for the current player */
+    @FXML private Text currentPlayer;
+
+    /**
+     * Initalize
+     * 
+     * JavaFX method to initalize an inital state for the screen.
+     */
+
+    @FXML
+    public void initialize() {
+        //get the login manager so we can see who is logged in
+        LoginManager loginManager = LoginManager.getInstance();
+        
+        //check if we are logged in
+        if(loginManager.isLoggedIn()) {
+            //get the username
+            String username = loginManager.getCurrentUser().getUsername();
+            //display it
+            currentPlayer.setText(username);
+        }
+    }
+
     public void newGame(ActionEvent e) throws IOException{
         System.out.println("new game");
         ScreenNavigator.switchScene(e, "fxml/game-screen.fxml");
@@ -22,6 +58,11 @@ public class PlayerScreenController {
 
     public void logOut(ActionEvent e) throws IOException{
         System.out.println("log out");
+
+        //get the login manager so we can log out
+        LoginManager loginManager = LoginManager.getInstance();
+        loginManager.logout();
+
         ScreenNavigator.switchScene(e, "fxml/login.fxml");
     }
 }
