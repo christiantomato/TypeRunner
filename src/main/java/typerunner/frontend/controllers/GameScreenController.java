c54a0d584e4f5fc4ec6a121524a7e4105161912f
Christian Tamayo <ctamayo@uwo.ca>
2026-04-01 22:44:42 -0400

    made the paragraph a textflow

--- src/main/java/typerunner/frontend/controllers/GameScreenController.java
+++ src/main/java/typerunner/frontend/controllers/GameScreenController.java
@@ -9,7 +9,9 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import typerunner.backend.GameEngine;
 import typerunner.backend.Word;
 import typerunner.frontend.ScreenNavigator;
@@ -29,9 +31,11 @@ public class GameScreenController implements Initializable {
     /** the current level user is on (high school, college, olympics) */
     @FXML private Text currLevel;
     /** the paragraph text that they need to type out */
-    @FXML private Text paragraph;
+    @FXML private TextFlow paragraph;
     /** the text area where they type out the words */
     @FXML private TextArea inputField;
+    /** the actual string of text they are typing */
+    private String targetText;
     /** boolean to make sure game has been setup before user starts typing */
     private boolean gameIsSetup;
 
@@ -65,35 +69,48 @@ public class GameScreenController implements Initializable {
      * 
      * Sets the game with the initial conditions.
      * 
+     * *String Builder Parsing with help of ChatGPT*
+     * 
      * @param e the button event
      */
 
     @FXML
-    private void setupGame(ActionEvent e){
-        paragraph.setText("");
+    private void setupGame(ActionEvent e) {
+        //clear before setting up
+        paragraph.getChildren().clear();
         System.out.println("start");
 
-        //get level and determine number of words to type
+        //get difficulty and num words
         ArrayList<Word> list = new ArrayList<>();
         int multiplier = GameEngine.getInstance().getLevel().getDifficulty();
         int numWords = BASE_WORDS * multiplier;
 
-        //create dictionary and get a random number
         Dictionary dictionary = new Dictionary();
         Random random = new Random();
+
+        //build the target text along with the words list
+        StringBuilder fullText = new StringBuilder();
+
         int randomIndex = random.nextInt(MAX);
-        
-        //get random words
         list.add(dictionary.getWordList().get(randomIndex));
-        paragraph.setText(list.get(0).getFullText());
-        for(int i=1; i < numWords; i++){
+        fullText.append(list.get(0).getFullText());
+
+        for(int i = 1; i < numWords; i++) {
             randomIndex = random.nextInt(MAX);
             list.add(dictionary.getWordList().get(randomIndex));
-            paragraph.setText(paragraph.getText() + " " + list.get(i).getFullText());
+            fullText.append(" ").append(list.get(i).getFullText());
         }
 
-        //set the setup flag
-        this.gameIsSetup = true;
+        //set the target text
+        targetText = fullText.toString();
+
+        //add it to the text flow
+        Text textNode = new Text(targetText);
+        paragraph.getChildren().add(textNode);
+
+        System.out.println(targetText);
+
+        gameIsSetup = true;
     }
 
     /**
@@ -103,13 +120,27 @@ public class GameScreenController implements Initializable {
      */
 
     @FXML
-    private void getKeyInput() {
+    private void getKeyInput(KeyEvent e) {
         if(gameIsSetup) {
-            System.out.println("key typed");
+            //get the text from the texfield
+            String textAreaString = inputField.getText();
 
-            //figure out which key has been typed
+            //get the key that has been typed
+            String inputChar = e.getCharacter();
+            System.out.println(inputChar);
+
             
-        }
+        }   
+    }
+
+    /**
+     * Update Paragraph Text
+     * 
+     * Gives feedback on characters that are being correctly or incorrectly typed
+     */
+
+    public void updateParagraphText(boolean correct, int index) {
+
     }
 
     /**
