2bb6e3208e7f5363eb780f912ebf2b6f578d1c73
Sahej Sethi <ssethi55@uwo.ca>
2026-04-03 20:00:47 -0400

    added descriptions for tutorials, and formatting etc -- TUTORIAL DONE

--- src/main/java/typerunner/frontend/controllers/TutorialController.java
+++ src/main/java/typerunner/frontend/controllers/TutorialController.java
@@ -36,7 +36,9 @@ public class TutorialController {
     private String[] descriptions = {
         "The first step in being able to play TypeRunner, is to create an account! To do so, first you must select on \"Admin Controls\" in the main menu, following this you must enter the games parental passkey; \"1234\".",
         "Once you've entered the passkey, you are taken to the admin control panel, from here we can click \"Create Account\" to make our first accounts. In the New Account page you'll need to enter a username and password, followed by an optional administrator overlooking the account, you may also, using the dropdown, select whether this account is for a player, or an administrator.",
-        ""
+        "Now that you've created you first account, you can click on Login within the main menu, and enter the username and password you created earlier, to login!",
+        "From here you've entered the Player Screen, to begin your first race, simply click Start New Game, followed by start in the top right corner, following this, a paragraph will appear on your screen, the goal is to type this paragraph out using your keyboard, as quickly as possible! make sure you type carefully, every 5 perfectly typed words, grants your player more health, and sets your opponents back on the track!",
+        "Once you've completed your first race, feel free to go back to the Player Screen and check out the Select Level page, where you can select the difficulty of the game. Every player begins at the high school level, needing to reach 40WPM to progress to the next level, and 60 to the level after that. if you'd like an even bigger challenge, feel free to click the hardcore button on the top left of the Game Screen to enable a no mistake gamemode."
     };
 
     private int currPage = 0;
@@ -47,6 +49,8 @@ public class TutorialController {
         leftButton.setDisable(true);
         leftImage.setImage(images[0]);
         rightImage.setImage(images[1]);
+        description.setText(descriptions[currPage]);
+        pageCount.setText(currPage+1 + "/" + 5);
     }
 
     @FXML
@@ -67,8 +71,10 @@ public class TutorialController {
 
         currPage++;
 
+        pageCount.setText(currPage+1 + "/" + 5);
         leftImage.setImage(images[currPage * 2]);
         rightImage.setImage(images[(currPage * 2) + 1]);
+        description.setText(descriptions[currPage]);
 
         if(currPage == MAX_PAGE){
             rightButton.setDisable(true);
@@ -81,8 +87,10 @@ public class TutorialController {
 
         currPage--;
 
+        pageCount.setText(currPage+1 + "/" + 5);
         leftImage.setImage(images[currPage * 2]);
         rightImage.setImage(images[(currPage * 2) + 1]);
+        description.setText(descriptions[currPage]);
 
         if(currPage == 0){
             leftButton.setDisable(true);
